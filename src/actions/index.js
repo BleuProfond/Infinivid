9b1dbc6e056d28dde2492d91dd37489039303a42
BleuProfond <p.verteuil@gmail.com>
2016-05-25 12:24:51 -0400

    installed redux thunk and connected to store

---
+++ src/actions/index.js
@@ -0,0 +1,15 @@
+
+
+export function signinUser({ email, password }) {
+  return function(dispatch) {
+    // Submit email/password to the server
+
+    // If request is valid...
+    // - Update state to indicate user is authenticated
+    // - Save the JWT token
+    // - redirect to the route '/feature'
+
+    // If request is invalid...
+    // - Show an error to the user
+  }
+}
